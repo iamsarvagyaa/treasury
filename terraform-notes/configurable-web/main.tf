040ab8582961fa00b09342058de138963b047db9
Sarvagya Sagar <iamsarvagyaa@gmail.com>
2022-06-22 13:21:32 +0530

    learning: 20220622 (#2)

--- terraform-notes/configurable-web/main.tf
+++ terraform-notes/configurable-web/main.tf
@@ -11,8 +11,30 @@
 #   default     = 78
 # }
 
-variable "list_example" {
-  description = "example of a list in terraform"
-  type        = list(any)
-  default     = ["a", "b", "c", "d", "e", "f"]
-}
+// here: example of an input variable that checks the value is a list
+
+# variable "list_example" {
+#   description = "example of a list in terraform"
+#   type        = list(any)
+#   default     = ["a", "b", "c", "d", "e", "f"]
+# }
+
+// here: example of an input variable that requires all the items in the list to be a numbers
+
+# variable "list_numeric_example" {
+#   description = "example of a numeric list"
+#   type        = list(number)
+#   default     = [1, 2]
+# }
+
+// here: example of a map that requires all values to be string.
+
+# variable "map_example" {
+#   description = "example of a map"
+#   type        = map(string)
+#   default = {
+#     key1 = "value1"
+#     key2 = "value2"
+#     key3 = "value3"
+#   }
+# }
